64a20695b36600adf0b185e58f5c59e22bc77726
anish-107 <anish.k7644@gmail.com>
2026-06-17 07:29:45 +0530

    CORS Fixed

--- app/web/Lipspeak/backend/app/main.py
+++ app/web/Lipspeak/backend/app/main.py
@@ -95,16 +95,6 @@ app.include_router(
     realtime_router,
 )
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 
 # Health Check
 @app.get("/")
